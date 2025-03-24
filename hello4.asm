3939cbc877b469f0136ae9fd5b22bed2a4196da4
MANAV AHIRE <rockstarsigma@gmail.com>
2025-03-24 10:32:51 +0530

    Add files via upload

--- hello4.asm
+++ hello4.asm
@@ -1,11 +1,3 @@
-%macro display 2
-    mov eax, 4            ; syscall number for sys_write in 32-bit mode
-    mov ebx, 1            ; file descriptor 1 (stdout)
-    mov ecx, %1           ; pointer to message
-    mov edx, %2           ; message length
-    int 0x80              ; invoke syscall
-%endmacro
-
 section .data
 rmodemsg db 10,"Processor is in Real Mode"
 rmsg_len equ $-rmodemsg
@@ -25,23 +17,32 @@ colmsg db ":"
 nwline db 10
 
 section .bss
-gdt resd 1      ; 4 bytes for the GDT
-ldt resw 1      ; 2 bytes for the LDT
-idt resd 1      ; 4 bytes for the IDT
-tr resw 1       ; 2 bytes for Task Register
-cr0_data resd 1 ; 4 bytes for Control Register 0
-dispbuff resb 04 ; 4 bytes buffer for display
+gdt resd 1
+resw 1
+ldt resw 1
+idt resd 1
+resw 1
+tr resw 1
+cr0_data resd 1
+dispbuff resb 04
+
+%macro display 2
+mov rax,1      ;syscall number for sys_write
+mov rdi,1      ;file descriptor (stdout/screen)
+mov rsi,%1     ;msg
+mov rdx,%2     ;msg_len
+syscall        ;make syscall
+%endmacro
 
 section .text
 global _start
-
 _start:
-    smsw eax              ; Store the MSW (bits 0-15 of control register CR0) into eax
-    mov [cr0_data], eax   ; Save it in memory
-    bt eax, 0             ; Check the PE (Protected Mode Enable) bit (LSB)
-    jc prmode             ; If PE = 1, jump to prmode
+    smsw eax         ;Store the MSW (bits 0 through 15 of control register CR0) into eax.
+    mov [cr0_data], eax
+    bt eax, 0        ;Checking PE (Protected Mode Enable) bit (LSB)
+    ;If PE bit is 1, it's Protected Mode; if 0, it's Real Mode.
+    jc prmode
 
-    ; Real mode
     display rmodemsg, rmsg_len
     jmp nxt1
 
@@ -49,73 +50,69 @@ prmode:
     display pmodemsg, pmsg_len
 
 nxt1:
-    ; sgdt, sldt, sidt, str are used to load the system descriptors and registers.
-    ; They require memory addresses. Use resd (4 bytes) for the GDT, IDT, etc.
-    
-    sgdt [gdt]            ; Store GDT in memory
-    sldt [ldt]            ; Store LDT in memory
-    sidt [idt]            ; Store IDT in memory
-    str [tr]              ; Store Task Register in memory
+    sgdt [gdt]       ; Store GDT (Global Descriptor Table) contents
+    sldt [ldt]       ; Store LDT (Local Descriptor Table) contents
+    sidt [idt]       ; Store IDT (Interrupt Descriptor Table) contents
+    str [tr]         ; Store Task Register contents
 
-    ; Display GDT contents
+    ; Display GDT Data
     display gdtmsg, gdtmsg_len
-    mov eax, [gdt+4]      ; Use eax to read 32-bit data from GDT
+    mov bx, [gdt + 4]
     call display16_proc
-    mov eax, [gdt+2]      ; Use eax to read 32-bit data from GDT
+    mov bx, [gdt + 2]
     call display16_proc
     display colmsg, 1
-    mov eax, [gdt]        ; Use eax to read 32-bit data from GDT
+    mov bx, [gdt]
     call display16_proc
 
-    ; Display LDT contents
+    ; Display LDT Data
     display ldtmsg, ldtmsg_len
-    mov ax, [ldt]         ; Use ax for 16-bit data from LDT
+    mov bx, [ldt]
     call display16_proc
 
-    ; Display IDT contents
+    ; Display IDT Data
     display idtmsg, idtmsg_len
-    mov eax, [idt+4]      ; Use eax to read 32-bit data from IDT
+    mov bx, [idt + 4]
     call display16_proc
-    mov eax, [idt+2]      ; Use eax to read 32-bit data from IDT
+    mov bx, [idt + 2]
     call display16_proc
     display colmsg, 1
-    mov eax, [idt]        ; Use eax to read 32-bit data from IDT
+    mov bx, [idt]
     call display16_proc
 
-    ; Display Task Register contents
+    ; Display Task Register Data
     display trmsg, trmsg_len
-    mov ax, [tr]          ; Use ax for 16-bit data from Task Register
+    mov bx, [tr]
     call display16_proc
 
-    ; Display Machine Status Word data
+    ; Display Machine Status Word (MSW) Data
     display mswmsg, mswmsg_len
-    mov ax, [cr0_data+2]  ; Use ax to read 16-bit data from MSW
+    mov bx, [cr0_data + 2]
     call display16_proc
-    mov eax, [cr0_data]   ; Use eax to read 32-bit data from MSW
+    mov bx, [cr0_data]
     call display16_proc
+
     display nwline, 1
+    mov rax, 60     ; Exit system call
+    mov rdi, 0      ; Exit status 0
+    syscall
 
-    ; Exit the program
-    mov eax, 1            ; syscall number for exit
-    mov ebx, 0            ; exit code 0
-    int 0x80              ; invoke syscall
-
-; Display procedure for 16-bit values
+;************************************************************
 display16_proc:
-    mov rdi, dispbuff     ; Point rdi to the buffer
-    mov rcx, 4            ; Load number of digits to display (4 hex digits)
+    mov rdi, dispbuff       ; Point rdi to buffer
+    mov rcx, 4              ; Load number of digits to display
 dispup1:
-    rol eax, 4            ; Rotate eax left by 4 bits
-    mov dl, al            ; Move lower byte into dl
-    and dl, 0fh           ; Mask upper nibble
-    add dl, 30h           ; Convert to ASCII
-    cmp dl, 39h           ; If it's <= 9, continue
+    rol bx, 4              ; Rotate number left by 4 bits
+    mov dl, bl             ; Move lower byte in dl
+    and dl, 0fh            ; Mask upper digit of byte in dl
+    add dl, 30h            ; Convert to ASCII
+    cmp dl, 39h            ; Compare with 39h
     jbe dispskip1
-    add dl, 7             ; If it's > 9, adjust for hex letters
+    add dl, 07h            ; If greater, add 07h to adjust ASCII
 dispskip1:
-    mov [rdi], dl         ; Store ASCII character in buffer
-    inc rdi               ; Move to next byte
-    loop dispup1          ; Repeat for all 4 digits
-    display dispbuff, 4    ; Display the buffer contents
+    mov [rdi], dl          ; Store ASCII code in buffer
+    inc rdi                ; Point to next byte
+    loop dispup1           ; Repeat for all digits
+    display dispbuff, 4     ; Display the final result
     ret
 
